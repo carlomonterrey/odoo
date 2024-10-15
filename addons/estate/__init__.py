e9ee3625f7d7edfcbea6990d01e4acb38ae5d577
carlomonterrey <carlosmonterrey17@gmail.com>
2024-10-14 22:50:43 -0400

    Add estate module

---
+++ addons/estate/__init__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'estable',
+    'category': 'estable',
+    'summary': 'Track leads and close opportunities',
+    'website': 'https://www.odoo.com/page/crm',
+    'depends': [
+        'base'
+       
+    ]}
---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'estate',
+    'category': 'Marketing',
+    'description': 'estate module',
+    'version': '0.1',
+    'depends': [
+        'base'
+    ],
+    
+    'license': 'LGPL-3',
+}
