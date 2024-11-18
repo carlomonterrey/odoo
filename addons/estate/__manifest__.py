9490a30cd62400ef9969a42278570a57143d29c2
carlomonterrey <carlosmonterrey17@gmail.com>
2024-11-18 15:07:47 -0500

    Add(estate module) estate prperty view form

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -13,8 +13,9 @@
         'application': True,
         'data':[
                  'security/ir.model.access.csv',
-
-        ],
+    'views/estate_property_views.xml',
+],
+       
 
     
     'license': 'LGPL-3',
